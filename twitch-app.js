58fb177192cb5bdd612074864a5d38fcd3717437
gitPhunky <jabayquen@gmail.com>
2018-03-25 23:21:44 -0700

    debug update
    update to view obj from api in console

--- twitch-app.js
+++ twitch-app.js
@@ -1,7 +1,8 @@
 window.onload = function() {
     //GET  https://wind-bow.glitch.me/twitch-api/streams/burkeblack
-    $.get("https://wind-bow.glitch.me/twitch-api/streams/burkeblack", function(data, status) {
-        alert("Data: " + data + "\nStatus: " + status);
-        console.log(data);
+    $.get("https://wind-bow.glitch.me/twitch-api/streams/streamerhouse", function(data, status) {
+        //alert("Data: " + data + "\nStatus: " + status);
+        console.log("Data: "); console.log(data);
+        console.log("Status:" + status);
       });
     };
