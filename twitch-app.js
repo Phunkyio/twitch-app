311af91058d7e25a64125c52a0227745dddd4cbc
gitPhunky <jabayquen@gmail.com>
2018-03-28 15:58:51 -0700

    display-array
    pushes data array streamerList to display using test template in
    index.html

--- twitch-app.js
+++ twitch-app.js
@@ -2,15 +2,19 @@ var streamerList = [];
 var streamerData, streamerStatus;
 streamerList.push('streamerhouse');
 streamerList.push('burkeblack');
+streamerList.push('summit1g');
 
-function testData(data, status){
+function testData(data, status) {
   console.log("Data:");
   console.log(data);
   console.log(status);
 }
 
-function displayStreamer(data){
-  var display = '';
+function displayStreamer(data) {
+  console.log(data.stream.channel.logo);
+  var display = "<div class='streamerDisplay'><div class='streamerProfile'><img src='" + data.stream.channel.logo + "' /><br /><span>" + data.stream.channel.display_name + "</span></div><div class='streamerDescription'><span>" + data.stream.channel.status + "</span></div></div></div>";
+
+  $(".streamerTable").append(display);
 }
 
 window.onload = function() {
@@ -19,8 +23,11 @@ window.onload = function() {
 
   for (var c = 0; c < streamerList.length; c++) {
     console.log(streamerList);
-    $.get("https://wind-bow.glitch.me/twitch-api/streams/" + streamerList[c], function(data, status){
+    $.get("https://wind-bow.glitch.me/twitch-api/streams/" + streamerList[c], function(data, status) {
       testData(data, status);
+      if (data.stream != null) {
+        displayStreamer(data);
+      } else {}
     });
 
   }
