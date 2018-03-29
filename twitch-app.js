65d74595bc1bbd15eb3320ecec2517005eff24c5
gitPhunky <jabayquen@gmail.com>
2018-03-28 18:03:06 -0700

    append streamers
    created input field and button to add streamers to the array.

--- twitch-app.js
+++ twitch-app.js
@@ -4,6 +4,18 @@ streamerList.push('streamerhouse');
 streamerList.push('burkeblack');
 streamerList.push('summit1g');
 
+function addStreamer(){
+  var inputStreamer = $('#inputStreamer').val();
+  console.log(inputStreamer);
+  $.get("https://wind-bow.glitch.me/twitch-api/streams/" + inputStreamer, function(data,status){
+    if (data.stream != null) {
+      displayStreamer(data);
+    } else {
+      console.log("no stream data");
+    }
+  });
+}
+
 function testData(data, status) {
   console.log("Data:");
   console.log(data);
@@ -11,18 +23,17 @@ function testData(data, status) {
 }
 
 function displayStreamer(data) {
-  console.log(data.stream.channel.logo);
   var display = "<div class='streamerDisplay'><div class='streamerProfile'><img src='" + data.stream.channel.logo + "' /><br /><span>" + data.stream.channel.display_name + "</span></div><div class='streamerDescription'><span>" + data.stream.channel.status + "</span></div></div></div>";
 
   $(".streamerTable").append(display);
 }
 
+//onload
 window.onload = function() {
   //GET  https://wind-bow.glitch.me/twitch-api/streams/burkeblack
 
 
   for (var c = 0; c < streamerList.length; c++) {
-    console.log(streamerList);
     $.get("https://wind-bow.glitch.me/twitch-api/streams/" + streamerList[c], function(data, status) {
       testData(data, status);
       if (data.stream != null) {
@@ -32,3 +43,23 @@ window.onload = function() {
 
   }
 };
+
+//handlers
+$('#addStreamer').click(function(){
+  addStreamer();
+  // var inputStreamer = $('#inputStreamer').val();
+  // console.log(inputStreamer);
+  // $.get("https://wind-bow.glitch.me/twitch-api/streams/" + inputStreamer, function(data,status){
+  //   if (data.stream != null) {
+  //     displayStreamer(data);
+  //   } else {
+  //     console.log("no stream data");
+  //   }
+  // });
+});
+
+$('#inputStreamer').keypress(function(e){
+  if(e.keyCode == 13){
+    addStreamer();
+  }
+});
