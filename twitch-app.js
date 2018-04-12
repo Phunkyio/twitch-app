a015ebcbe7bb3bbe00126be74f49bf3d7ec1c6c7
gitPhunky <jabayquen@gmail.com>
2018-04-11 18:13:55 -0700

    duplicate streamer test
    test to see if streamer being added to the array already exists

--- twitch-app.js
+++ twitch-app.js
@@ -1,8 +1,9 @@
+var inputStreamer;
 var streamerList = [];
 var streamerData, streamerStatus;
+var duplicateTest;
 
-function addStreamer() {
-  var inputStreamer = $('#inputStreamer').val();
+function addStreamer(inputStreamer) {
   streamerList.push(inputStreamer);
   // TODO: Purge invalid Twitch IDs.
   console.log(inputStreamer);
@@ -27,7 +28,7 @@ function testData(data, status) {
 }
 
 function displayStreamer(data) {
-  var display = "<div class='streamerDisplay'><div class='streamerProfile'><a href="+data.stream.channel.url+"><img src='" + data.stream.channel.logo + "' /></a><br /><span>" + data.stream.channel.display_name + "(" + data.stream.channel.broadcaster_language + ")</span></div><div class='streamerDescription'><div><img src=" + data.stream.preview.medium + "/></div><div>" + data.stream.channel.status + "</div><div>" + data.stream.game + "</div><div>" + data.stream.viewers + " viewers</div></div></div></div>";
+  var display = "<div class='streamerDisplay'><div class='streamerProfile'><a href=" + data.stream.channel.url + "><img src='" + data.stream.channel.logo + "' /></a><br /><span>" + data.stream.channel.display_name + "(" + data.stream.channel.broadcaster_language + ")</span></div><div class='streamerDescription'><div><img src=" + data.stream.preview.medium + "/></div><div>" + data.stream.channel.status + "</div><div>" + data.stream.game + "</div><div>" + data.stream.viewers + " viewers</div></div></div></div>";
 
   $(".streamerTable").append(display);
 }
@@ -56,6 +57,25 @@ function streamerDisplayRefresh() {
   console.log(streamerList);
 }
 
+function streamerDuplicateCheck(inputStreamer) {
+  console.log("attempting duplicate check for: " + inputStreamer);
+  var duplicateTestPass = true;
+  if (streamerList.length == 0) {
+    return true;
+  } else {
+    for (var x = 0; x < streamerList.length; x++) {
+      console.log("Evaluating " + inputStreamer + " against " + streamerList[x]);
+      if (inputStreamer == streamerList[x]) {
+        console.log("Duplicate check fail.");
+        return false;
+      } else {
+        console.log("Duplicate check pass.");
+      }
+    }
+    return true;
+  }
+}
+
 //onload
 window.onload = function() {
   //GET  https://wind-bow.glitch.me/twitch-api/streams/burkeblack
@@ -64,7 +84,11 @@ window.onload = function() {
 
 //handlers
 $('#addStreamer').click(function() {
-  addStreamer();
+  inputStreamer = $("#inputStreamer").val();
+  duplicateTest = streamerDuplicateCheck(inputStreamer);
+  if (duplicateTest) {
+    addStreamer(inputStreamer);
+  }
   // var inputStreamer = $('#inputStreamer').val();
   // console.log(inputStreamer);
   // $.get("https://wind-bow.glitch.me/twitch-api/streams/" + inputStreamer, function(data,status){
@@ -78,7 +102,11 @@ $('#addStreamer').click(function() {
 
 $('#inputStreamer').keypress(function(e) {
   if (e.keyCode == 13) {
-    addStreamer();
+    inputStreamer = $("#inputStreamer").val();
+    duplicateTest = streamerDuplicateCheck(inputStreamer);
+    if (duplicateTest) {
+      addStreamer(inputStreamer);
+    }
   }
 });
 
